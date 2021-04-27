bee3495226b909f4afad9cbdb29fd2776cd43007
zem <zemzelett@users.noreply.github.com>
2021-04-27 23:48:50 +0200

    cfg-cam: Prepped, does not work yet

---
+++ automatic/cfgcam/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$pkgDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = ''
+$checksum = ''
+
+
+# $packageArgs = @{
+#   PackageName  = $env:ChocolateyPackageName
+#   FileFullPath = "$pkgDir\WebCameraConfig.exe"
+#   Url          = $url
+#   Checksum     = $checksum
+#   ChecksumType = 'sha256'
+# }
+
+#Get-ChocolateyWebFile $packageArgs -FileFullPath $packageArgs.FileFullPath
+Get-ChocolateyWebFile -packageName $env:ChocolateyPackageName -FileFullPath "$pkgDir\WebCameraConfig.exe" -Url $url -Checksum $checksum
---
+++ automatic/cfgcam/update.ps1
@@ -0,0 +1,29 @@
+import-module au
+
+$releases = 'https://api.github.com/repos/SuslikV/cfg-cam/releases/latest'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $json = Invoke-RestMethod -Uri $releases
+
+    foreach ($asset in $json.assets) {
+        if ($asset.name -match "exe") {
+            $url = $asset.browser_download_url
+        }
+    }
+    
+    $version = $url -Split "/" | Select-Object -Last 1 -Skip 1
+
+    $Latest = @{ URL = $url; Version = $version }
+    return $Latest
+}
+
+update -CheckSumFor 32
