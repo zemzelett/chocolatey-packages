6b234e0cea0557f6483bf8ed6271fc0cd0b036a7
zem <zemzelett@users.noreply.github.com>
2021-05-08 12:49:37 +0200

    BGB: added deletion of install dir to uninstall

--- automatic/bgb/tools/chocolateyUninstall.ps1
+++ automatic/bgb/tools/chocolateyUninstall.ps1
@@ -1,4 +1,8 @@
+$pkgDir = $env:ChocolateyInstall + "\lib\" + $env:ChocolateyPackageName
+$installDir = $pkgDir + "\bgb"
 $programsPath = [environment]::GetFolderPath([environment+specialfolder]::Programs)
 $shortcutPath = $programsPath + '\BGB.lnk'
 
 Remove-Item -Force $shortcutPath
+# Necessary as ini files would stay on disk otherwise!
+Remove-Item -Recurse -Force $installDir
